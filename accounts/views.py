a205d4726904623296529b75355f2090da0bf41c
Bikram <ambreeshpatel98@gmail.com>
2024-08-01 12:52:51 +0530

    added logout view

--- accounts/views.py
+++ accounts/views.py
@@ -71,6 +71,11 @@ def login_view(request):
     return render(request, "login.html", {"form": form})
 
 
+def logout_view(request):
+    logout(request)
+    return redirect('home') 
+
+
 # Dummy data to simulate database records
 appointments_data = [
     {
