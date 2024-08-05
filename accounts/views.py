eb21c83e091a628947701885f65670d5172260ba
Bikram <ambreeshpatel98@gmail.com>
2024-08-05 14:22:02 +0530

    updated patient model add forms

--- accounts/views.py
+++ accounts/views.py
@@ -16,7 +16,7 @@ from .forms import (
     AppointmentForm,
     EditDoctorProfileForm,
 )
-from .models import Doctor, Appointment,CustomUser,Patient
+from .models import Doctor, Appointment,CustomUser
 import json
 from .forms import SignUpForm
 from django.contrib.auth.models import Group
@@ -227,34 +227,28 @@ def doctor_dashboard_view(request):
 
 @login_required
 def book_appointment(request, doctor_id):
-    doctor = get_object_or_404(CustomUser, id=doctor_id, user_type='Doctor')
-    
+    doctor = get_object_or_404(Doctor, id=doctor_id)
     if request.method == "POST":
-        form = AppointmentForm(request.POST)
-        if form.is_valid():
-            # Ensure the user has a related Patient profile
-            try:
-                patient_profile = Patient.objects.get(user=request.user)
-            except Patient.DoesNotExist:
-                messages.error(request, "Patient profile not found.")
-                return redirect("error_page")  # Adjust this to your actual error handling URL
+        date = request.POST.get("date")
+        time = request.POST.get("time")
+        status = request.POST.get('status')
 
-            # Create the appointment
-            Appointment.objects.create(
-                doctor=doctor,
-                patient=request.user,  # Use CustomUser instance directly
-                date=form.cleaned_data["date"],
-                time=form.cleaned_data["time"],
-                status=form.cleaned_data["status"]
-            )
+        # Ensure the user has a related Patient profile
+        try:
+            patient = Patient.objects.get(user=request.user)
+        except Patient.DoesNotExist:
+            messages.error(request, "Patient profile not found.")
+            return redirect(
+                "error_page"
+            )  # Adjust this to your actual error handling URL
 
-            messages.success(request, "Appointment booked successfully!")
-            return redirect("doctor_dashboard")  # Adjust redirection as needed
+        # Create the appointment
+        Appointment.objects.create(doctor=doctor, patient=patient, date=date, time=time, status=status)
 
-    else:
-        form = AppointmentForm()
+        messages.success(request, "Appointment booked successfully!")
+        return redirect("doctor_dashboard")  # Adjust redirection as needed
 
-    return render(request, "book_appointment.html", {"doctor": doctor, "form": form})
+    return render(request, "book_appointment.html", {"doctor": doctor})
 
 
 def add_doctor(request):
