80f0c7a8da459c193f6232eaa97b113101957540
Bikram <ambreeshpatel98@gmail.com>
2024-08-08 02:07:38 +0530

    updated AppointmentForm

--- accounts/views.py
+++ accounts/views.py
@@ -215,14 +215,16 @@ def about(request):
 def doctor_dashboard_view(request):
     doctor = get_object_or_404(Doctor, user=request.user)
     
-    
+    appointments = Appointment.objects.filter(doctor=doctor)
+
     # Fetch all doctors (or adjust the query as needed)
     doctors = Doctor.objects.all()
     user = CustomUser.objects.get(id=request.user.id)
 
     context = {
+        'appointments': appointments,
         'doctors': doctors,
-        'user': user,
+        'user': request.user,
     }
     
     return render(request, 'doctor_dashboard.html', context)
