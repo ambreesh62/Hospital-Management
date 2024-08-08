d82ec28f11ead80952417ccb760870d7e9fd4d84
Bikram <ambreeshpatel98@gmail.com>
2024-08-09 01:39:27 +0530

    upadated

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from .views import (
     view_doctor_view,
     edit_doctor_view,
     delete_doctor_view,
-    # profile_view,
+    profile_view,
     signup_view,
     login_view,
     home_view,
@@ -16,9 +16,7 @@ from .views import (
     error_page,
     about,
     edit_blog_post,
-    patient_profile_view,
-    doctor_profile_view,
-
+    
 )
 
 urlpatterns = [
@@ -28,15 +26,15 @@ urlpatterns = [
     path("doctor_dashboard/", doctor_dashboard_view, name="doctor_dashboard"),
     path("patient_dashboard/", patient_dashboard_view, name="patient_dashboard"),
     path("error/", error_page, name="error_page"),
-    # path("profile/", profile_view, name="profile"),
-    # path("profile/<int:doctor_id>/", profile_view, name="profile"),
+    path("profile/", profile_view, name="profile"),
+    path("profile/<int:doctor_id>/", profile_view, name="profile"),
     path("logout/", logout_view, name="logout"),
     path("view_doctor/<int:doctor_id>/", view_doctor_view, name="view_doctor"),
     path("edit_doctor/<int:doctor_id>/", edit_doctor_view, name="edit_doctor"),
     path("delete_doctor/<int:doctor_id>/", delete_doctor_view, name="delete_doctor"),
     path("update_profile/", update_doctor_profile_view, name="update_doctor_profile"),
     path("about/", about, name="about"),
-    # path('profile/<int:doctor_id>/', views.profile_view, name='doctor_profile'),  # Handles a specific doctor's profile
+    path('profile/<int:doctor_id>/', views.profile_view, name='doctor_profile'),  # Handles a specific doctor's profile
 
     # new
     path('create_blog_post/', views.create_blog_post, name='create_blog_post'),
@@ -56,8 +54,6 @@ urlpatterns = [
     path('appointment/<int:appointment_id>/accept/', views.accept_appointment, name='accept_appointment'),
     path('appointment/<int:appointment_id>/cancel/', views.cancel_appointment, name='cancel_appointment'),
     path('patient-profile/<int:id>/', patient_profile_view, name='patient_profile'),
-    path('doctor-profile/<int:id>/', doctor_profile_view, name='doctor_profile'),
-
 
 
 
