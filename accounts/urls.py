b87e7e862226a80987b5010dee6acceba911dcd3
Bikram <ambreeshpatel98@gmail.com>
2024-08-11 14:49:50 +0530

    added patient profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -55,6 +55,7 @@ urlpatterns = [
     path('appointment/<int:appointment_id>/cancel/', views.cancel_appointment, name='cancel_appointment'),
     path('profile/<int:patient_id>/', views.profile_view , name='patient_profile'),
     path('patient/edit/<int:id>/', edit_patient_profile, name='edit_patient_profile'),
+    path('profile/<int:patient_id>/', profile_view , name='patient_profile'),
 
 
 
